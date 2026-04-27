465a68da5e163e3f2744e8c48d87e77901715ea5
Paul Martins <vmartins.paul@gmail.com>
2026-04-27 18:14:45 +0100

    Added test for Identity Attribute

---
+++ src/Muify.Tests/Domain/IdentityAttributeStrategyTests/WhenApplyIsCalled.cs
@@ -0,0 +1,33 @@
+﻿namespace Muify.Domain.IdentityAttributeStrategyTests;
+
+using Muify;
+
+public sealed class WhenApplyIsCalled
+{
+    [Test]
+    public async Task GivenNothingThenTheIdentityAttributeShouldBeReturned()
+    {
+        // Arrange
+        const string expected = """
+            namespace Muify.Domain
+            {
+                [global::System.AttributeUsageAttribute(global::System.AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
+                [global::Microsoft.CodeAnalysis.EmbeddedAttribute]
+                public sealed partial class IdentityAttribute
+                    : global::System.Attribute
+                {
+                }
+            }
+            """;
+
+        var strategy = new IdentityAttributeStrategy();
+
+        // Act
+        IEnumerable<File> result = strategy.Apply();
+
+        // Assert
+        File identity = await Assert.That(result).HasSingleItem();
+        _ = await Assert.That(identity.Content).IsEqualTo(expected);
+        _ = await Assert.That(identity.Hint).IsEqualTo(IdentityAttributeStrategy.Hint);
+    }
+}
