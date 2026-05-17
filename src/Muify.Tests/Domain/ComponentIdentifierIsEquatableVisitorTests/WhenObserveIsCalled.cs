2480b4f4eeafb0dc0516c4506c85452033720672
Paul Martins <vmartins.paul@gmail.com>
2026-05-17 17:07:38 +0100

    Added Identifier Equatable generation

---
+++ src/Muify.Tests/Domain/ComponentIdentifierIsEquatableVisitorTests/WhenObserveIsCalled.cs
@@ -0,0 +1,64 @@
+﻿namespace Muify.Domain.ComponentIdentifierIsEquatableVisitorTests;
+
+using Mu.Modelling;
+using Mu.Modelling.Testing;
+using Muify;
+
+public sealed class WhenObserveIsCalled
+{
+    [Test]
+    public async Task GivenAUnitWhenIdentifierHasEquatableIsFalseThenEquatableToIdentifierDefinitionIsGenerated()
+    {
+        // Arrange
+        const string expected = """
+            namespace MooVC.Testing.Mechanics.Car
+            {
+                public sealed partial class Wheel
+                    : global::System.IEquatable<global::MooVC.Testing.Mechanics.Car.Locations>
+                {
+                }
+            }
+            """;
+
+        var visitor = new ComponentIdentifierIsEquatableVisitor();
+        Component wheel = TestData.Single.Units.Value[0].Components[1].WithMetadata(metadata => metadata.WithSelf(self => self.IsEquatable(false)));
+        Model.Graph.Areas.Area.Units.Unit.Components.Component component = new(TestData.Single.Components, 0, TestData.Single.Model, wheel);
+
+        // Act
+        IEnumerable<File> result = visitor.Observe(component);
+
+        // Assert
+        File definition = await Assert.That(result).HasSingleItem();
+        _ = await Assert.That(definition.Content).IsEqualTo(expected);
+        _ = await Assert.That(definition.Hint).IsEqualTo(wheel.Name);
+    }
+
+    [Test]
+    public async Task GivenAUnitWhenSelfHasEquatableThenNothingIsGenerated()
+    {
+        // Arrange
+        var visitor = new ComponentIdentifierIsEquatableVisitor();
+        Component wheel = TestData.Single.Units.Value[0].Components[1].WithMetadata(metadata => metadata.WithSelf(self => self.IsEquatable(true)));
+        Model.Graph.Areas.Area.Units.Unit.Components.Component component = new(TestData.Single.Components, 0, TestData.Single.Model, wheel);
+
+        // Act
+        IEnumerable<File> result = visitor.Observe(component);
+
+        // Assert
+        _ = await Assert.That(result).IsEmpty();
+    }
+
+    [Test]
+    public async Task GivenAComponentWhenOutOfScopeThenNothingIsGenerated()
+    {
+        // Arrange
+        var visitor = new ComponentIdentifierIsEquatableVisitor();
+        Model.Graph.Areas.Area.Units.Unit.Components.Component component = TestData.Single.Wheel;
+
+        // Act
+        IEnumerable<File> result = visitor.Observe(component);
+
+        // Assert
+        _ = await Assert.That(result).IsEmpty();
+    }
+}
